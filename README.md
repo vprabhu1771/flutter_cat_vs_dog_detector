37674fb407daf5b0aabd8c4158450536a5558ac9
vprabhu1771 <vprabhu1771@gmail.com>
2024-12-03 18:05:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_cat_vs_dog_detector
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_cat_vs_dog_detector/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_cat_vs_dog_detector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
